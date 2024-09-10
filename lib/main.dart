45a3899b5e804ed8b9507c9b12d63381c07c05e7
hamiwhitney <hamiwhitney@gmail.com>
2024-09-10 00:28:44 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+home: WeatherScreen(), );
+}
+}
+
+class WeatherScreen extends StatefulWidget {
+@override
+
+_WeatherScreenState createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+String weatherInfo = 'Pressthe button to get weather info';
+
+void fetchWeather() async {
+final response = await
+http.get(Uri.parse('https://api.weatherapi.com/v1/current.json?key=YOUR_API_KEY&q=London'));
+
+if (response.statusCode == 200) {
+var data = jsonDecode(response.body);
+setState(() {
+weatherInfo = 'Weather in London: ${data['current']['temp_c']}°C';
+});
+} else {
+setState(() {
+weatherInfo = 'Failed to fetch weather data';
+});
+}
+}
+
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+
+appBar: AppBar(title: Text('Weather App')), body: Center(
+child: Column(
+mainAxisAlignment: MainAxisAlignment.center, children: [
+Text(weatherInfo, textAlign: TextAlign.center), SizedBox(height: 20), ElevatedButton(
+onPressed: fetchWeather, child: Text('Get Weather'), ), ], ), ), );
+}
+}
